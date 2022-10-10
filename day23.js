b09948f4aed5549c48660306630750de85ef01a8
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-10-10 19:48:13 +0530

    Add files via upload

---
+++ day23.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('#btn1');
+
+btn=document.addEventListener('click',()=>{
+    window.history.back();
+});
+// let btn1=document.querySelector('#takeback');
+// btn1.addEventListener('click',()=>{
+//     window.history.back();
+// });
