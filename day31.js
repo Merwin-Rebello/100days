38e9a374d3c106513c8e44aecbcb58ba91821f5e
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-10-18 15:20:03 +0530

    Add files via upload

---
+++ day31.js
@@ -0,0 +1,6 @@
+Sub = document.querySelector('btn1')
+names=document.getElementById('char')
+Sub.addEventListener('click', ()=>{
+     data = document.getElementById('char').value
+    alert(data)
+})
