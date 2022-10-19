a7918bbe13c3722e0c038f4de06ace778c72f0e9
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-10-19 23:17:23 +0530

    Update day31.js

--- day31.js
+++ day31.js
@@ -1,6 +1,7 @@
 Sub = document.querySelector('btn1')
-names=document.getElementById('char')
+name=document.getElementById('char')
 Sub.addEventListener('click', ()=>{
      data = document.getElementById('char').value
     alert(data)
 })
+// input tag is not called here
