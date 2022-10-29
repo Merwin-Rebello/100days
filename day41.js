bd2a9cb627c89bde965280761d1da6e356fb29c8
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-10-29 22:32:15 +0530

    Add files via upload

---
+++ day41.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'MERWIN',
+    walk() {},
+    talk() {}
+};
+
+person.talk();
+person.name = ''
+
+const targetValue = 'name'
+person[targetValue.value] = 'BEST'
+console.log(person)
