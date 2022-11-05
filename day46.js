b30db2d506e5101e5e9740def4141df975d13143
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-11-05 22:22:25 +0530

    Add files via upload

---
+++ day46.js
@@ -0,0 +1,14 @@
+ReactDOM.render(
+<p>first react code</p>,
+document.getElementById('root')
+)
+
+ReactDOM.render(
+<p>My name is MERWIN REBELLO</p>,
+document.getElementById('root')
+)
+
+ReactDOM.render(
+    <ul><li>MICROSOFT</li><li>LINUX</li></ul>,
+    document.getElementById('root')
+)
