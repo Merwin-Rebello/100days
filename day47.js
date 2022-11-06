d07e9a77a4c023d411a0a748d382ab95978e2b4f
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-11-06 20:40:01 +0530

    Add files via upload

---
+++ day47.js
@@ -0,0 +1,41 @@
+function maincontent(){
+    return(<div>
+        <h1>my react website</h1>
+        <h2> Merwin Rebello here!!!</h2>
+        <p> using funcions in react for the first time</p>
+    </div>
+    )
+}
+
+function main2(){
+   return( <div>
+        <h2> INDIA will win the world cup in 2022</h2>
+        <ul>
+            <li>i will be successfull</li>
+            <li> i am happy and will follow my dream</li>
+        </ul>
+    </div>
+   )
+}
+//in react use 'className' instead of 'class'
+function Body(){
+    return(
+         <div>
+         <h1 className='header'>this is my first react website usinf className</h1> 
+         </div>
+    )
+ }
+
+
+
+ReactDOM.render(
+    <div>
+        <maincontent />
+        <main2 />
+        <Body />
+    </div>,
+    document.getElementById('main')
+
+
+)
+
