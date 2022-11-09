bdea4442882d1fc5286e1f943c4a82e6e60aff75
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-11-09 21:06:46 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,22 @@
+#Create a class named MyClass, with a property named x
+'''class MyClass:
+  x = 5
+p1 =  MyClass()
+print(p1.x)'''
+
+#from typing_extensions import Self
+
+
+class Merwin:
+ def intellegent(self):
+    print(" intellegent student")
+boy1=Merwin()
+boy1.intellegent()    
+        
+     
+        
+        
+        
+
+     
+  
