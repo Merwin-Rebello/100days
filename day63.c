ae119ad97ae6d7bdd8a28920ff39f379ba0da6b5
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-11-22 22:20:31 +0530

    Add files via upload

---
+++ day63.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter your birth year");
+    scanf("%d",&num);
+    int sum = 2022-num;
+    printf("Your age is %d",sum); 
+    return 0;    
+}
