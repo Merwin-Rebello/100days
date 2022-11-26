f354f5512f3c679faa6b2662c5f90334c7602d61
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-11-26 21:21:15 +0530

    Add files via upload

---
+++ day66.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+ {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
+
+   // leap year if perfectly divisible by 400 but divisible by 4
+   if (year % 400 == 0 && year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   // not a leap year if divisible by 100
+   // but not divisible by 400
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   // leap year if not divisible by 100
+   // all other years are not leap years
+   else {
+      printf("%d is not a leap year.", year);
+   }
+
+   return 0;
+}
