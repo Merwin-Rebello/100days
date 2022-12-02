9c94fd5eee8d1b8f1c09033168e7b60027ae3762
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-12-02 13:13:56 +0530

    made changes in the header file

---
+++ d70.h
@@ -0,0 +1,16 @@
+void add(int x, int y)
+{
+    printf("%d",a+b);
+}
+void sub(int x, int y)
+{
+    printf("%d",a-b);
+}
+ void mul(int x , int y)
+ {
+    printf("%d",a*b);
+ }
+ void div(int x , int y)
+ {
+    printf("%d",a/b);
+ }
