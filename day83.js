d2dcfa72171d208ba3ac1cb42c7f0a84782a1913
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-12-15 23:59:57 +0530

    Add files via upload

---
+++ day83.js
@@ -0,0 +1,10 @@
+//window object - is a global object
+console.log(window)
+
+window.console.log(window)
+console.log(document.body)
+
+document.body.style.background="yellow"//we can also style the css thing using javascipt as well foreg images can also be added
+
+
+//BOM
