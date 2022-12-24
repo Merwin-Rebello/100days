4b4c5a077fae69f54249855b1227d0eba367171f
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2022-12-24 19:00:15 +0530

    Add files via upload

---
+++ day92.py
@@ -0,0 +1,8 @@
+# Python program to convert decimal into other number systems
+# dec = 344
+num = int(input("enterr dec number"))
+
+print("The decimal value of", num, "is:")
+print(bin(num), "in binary.")
+print(oct(num), "in octal.")
+print(hex(num), "in hexadecimal.")
