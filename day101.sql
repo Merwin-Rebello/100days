eafd81c2f5aa9452d44a75a63de1085280d9b340
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-01-03 13:36:58 +0530

    Add files via upload

---
+++ day101.sql
@@ -0,0 +1,16 @@
+-- create database firstdatabase  creation of a new database 
+-- use firstdatabase; -- using or setting the default database
+-- create table personal(
+-- Id INT,
+-- NAME varchar(40),
+-- DOB DATE,
+-- MOBILEno varchar(12),
+-- gender varchar(1)
+-- );
+
+CREATE TABLE PRODUCTS(
+prd_ID INT,
+prd_name varchar(15),
+prd_quantity char,
+orderid int
+);
