7881abb6d64ede86c89910cd368ec5623232b1db
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-01-04 09:45:35 +0530

    Add files via upload

---
+++ day102.sql
@@ -0,0 +1,16 @@
+-- INSERT INTO personal (Id , NAME , DOB , MOBILEno, gender)
+
+-- VALUES (1 ,"Merwin Rebello", "2004-04-02","5793927272","M");
+
+-- VALUES (3 ,"Andrew Tate","1980-05-15","12384848902","M"); this helps to add new row but insert function has to used every time
+
+-- For adding more rows or data we have to edited only  the main values which means that the values keyword should be written only once 
+-- and we have to edit that only to get a new row of record
+
+-- FOR ADDING MULTIPLE ROWS
+
+INSERT INTO personal (Id , NAME , DOB , MOBILEno, gender)
+VALUES
+(1 ,"Merwin Rebello", "2004-04-02","5793927272","M"),
+(2,"OLDIN REBELLO" , "1971-10-07","9820634849","M"),
+(3,"UMA REBELLO","1979-08-09","91858392974","F");
