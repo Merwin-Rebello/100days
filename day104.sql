4949400c0b716afd77d8f33f7ed47744ce99a590
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-01-10 22:16:26 +0530

    Add files via upload

---
+++ day104.sql
@@ -0,0 +1,8 @@
+-- SELECT * FROM personal; -- TO SEE ALL THE DATA OF THE TABLE-NAME PERSONAL...
+-- SELECT  Id, NAME, DOB FROM  personal; -- TO SEE SPECIFIC COLOUMNS FROM THE TABLE
+
+-- SELECT * FROM constrains
+-- WHERE gender ="FEmale"; -- this will only give the the record of the specific  condition which we have written 
+-- WHERE AGE <20;
+SELECT NAME ,AGE FROM constrains
+WHERE AGE <=20;
